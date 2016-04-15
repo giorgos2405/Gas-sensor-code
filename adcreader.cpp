4803b2b8c2dfd10bf3faa04e2ecdf2600bd79d2e
giorgos2405 <giorgos2405@hotmail.gr>
2016-04-15 13:42:40 +0100

    Update adcreader.cpp

--- adcreader.cpp
+++ adcreader.cpp
@@ -56,6 +56,24 @@ static void writeReset(int fd)
     pabort("can't send spi message");
 }
 
+static void writeReg(int fd, uint8_t v)
+{
+  int ret;
+  uint8_t tx1[1];
+  tx1[0] = v;
+  uint8_t rx1[1] = {0};
+  struct spi_ioc_transfer tr;
+
+  memset(&tr,0,sizeof(struct spi_ioc_transfer));
+  tr.tx_buf = (unsigned long)tx1;
+  tr.rx_buf = (unsigned long)rx1;
+  tr.len = sizeof(tx1);
+
+  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+  if (ret < 1)
+    pabort("can't send spi message");
+}
+
 static uint8_t readReg(int fd)
 {
         int ret;
@@ -78,9 +96,24 @@ static uint8_t readReg(int fd)
 
 static int readData(int fd)
 {
-  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-  if (ret < 1)
-    pabort("can't send spi message");
+	int ret;
+	uint8_t tx1[2] = {0,0};
+	uint8_t rx1[2] = {0,0};
+	struct spi_ioc_transfer tr;
+
+	memset(&tr,0,sizeof(struct spi_ioc_transfer));
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = sizeof(tx1);
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+          {
+	  printf("\n can't send spi message, ret = %d\n",ret);
+          exit(1);
+          }
+	  
+	return (rx1[0]<<8)|(rx1[1]);
 }
 
 static uint8_t readReg(int fd)
